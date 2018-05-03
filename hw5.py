c3e5b6dec1d309d7d5cf87693d5c5439e68cfbf2
tylergarrett <tjg3ea@virginia.edu>
2018-05-03 16:42:45 -0400

    A lot of stuff

--- hw5.py
+++ hw5.py
@@ -26,22 +26,89 @@ def save_data(info):
     f.close()
 
 # Phase One - Testing the slot machines
-# Strategy: idk 
+# Strategy: Test each slot 10 times since we have 1,000,000 credits
 def play_test_slots(state):
-	slot = 0
 
-	# Generic return
+	# Stop pulling after 1000 tests
+	if (state["pulls-left"] < 9000):
+		return {"team-code":state["team-code"], 
+			"game":state["game"],
+			"pull":None
+	}	
+
+	# Load in data
+	info = load_data()
+
+	# First time
+	if len(info) == 0:
+		# List of tested slots
+		info["slots-tested"] = []
+		# Dictionary of slots with list of payoffs
+		info["payoffs"] = {}
+		# Dict of slots with costs
+		info["costs"] = {}
+		save_data(info)
+		return {"team-code":state["team-code"], 
+				"game":state["game"],
+				"pull":slot
+		}
+
+	# Log data from returned state after first pull
+	info["slots-tested"] += slot
+	info["payoffs"][slot] += state["last-payoff"]
+	info["costs"][slot] = state["last-cost"]
+	save_data(info)
+
+	# Test each slot 10 times - increment when reached
+	if (info["slots-tested"].count(slot) = 10) and (slot < 100):
+		slot++
+
+	# Return statement
 	return {"team-code":state["team-code"], 
 			"game":state["game"],
 			"pull":slot
 	}
 
+# Pick best slots from testing phase
+def choose_slots():
+	slot_list = []
+	# Load data
+	info = load_data()
+	info["slot-ratios"] = {}
+
+	# Get average payoff ratio for each slot
+	for i in range(0,100):
+		average_payoff = info["payoffs"][i] / 10
+		ratio = average_payoff / info["costs"][i]
+		info["slot-ratios"][i] = ratio 
+	
+	# save
+	save_data(info)
+	
+	# Need to get top 10 ratios
+	for i in range(0,10):
+		key_to_choose = get_max_in_dict(info["slot-ratios"])
+		slot_list.append(key_to_choose)
+		info["slot-ratios"][key_to_choose] = 0
+		save_data(info)
+	return slot_list
+
+def get_max_in_dict(dict):
+	# Citing source: https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+	values = dict.values() # this gets all values
+	keys = dict.keys() # this gets all keys
+	return keys[values.index(max(values))] # this selects the key based on the max value in the list of avalues
+
+
 # Phase 2A - Choosing slots to auction
 # We get to choose 10
 def play_auction(state):
-	slot_list = []
+	info = load_data()
 
-	# Generic return
+	# Load slots chosen prior
+	slot_list = choose_slots()
+
+	# Return
 	return {"team-code":state["team-code"],
 			"game":state["game"],
 			"auctions":slot_list
