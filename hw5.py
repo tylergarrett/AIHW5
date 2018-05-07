7cf1ca1ca57d0400bad720fad1c0679901ab8f4d
bkellogg7 <brk7vu@virginia.edu>
2018-05-06 20:40:02 -0400

    lot of improvements and changes

--- hw5.py
+++ hw5.py
@@ -25,16 +25,27 @@ def save_data(info):
     f.flush()
     f.close()
 
+def get_best(info):
+	ratio = []
+
+	# Get average payoff ratio for each slot
+	for i in range(0,100):
+		ratio.append( (sum(info['payoffs'][str(i)])/len(info['payoffs'][str(i)])) / (info["costs"][str(i)] if info['costs'][str(i)] != 0 else .1))
+	# Need to get top 10 ratios
+	slot_list= sorted(range(len(ratio)), key=lambda i: ratio[i])[-15:]
+	slot_list.reverse()
+	return slot_list
+
 # Phase One - Testing the slot machines
 # Strategy: Test each slot 10 times since we have 1,000,000 credits
 def play_test_slots(state):
-		
 
 	# Load in data
 	info = load_data()
 
 	# First time
 	if len(info) == 0:
+		info['state'] = 'initial'
 		info['current_slot'] = '0'
 		info['trials'] = 0
 		info['money'] = 1000000
@@ -47,47 +58,56 @@ def play_test_slots(state):
 				"game":state["game"],
 				"pull":0
 		}
-	# Stop pulling after 1000 tests
-	if (state["pulls-left"] < 5000 or int(info['current_slot']) > 99):
+	if(info['money'] < 700000 and len(info['payoffs']) == 100):
 		return {"team-code":state["team-code"], 
 			"game":state["game"],
 			"pull":None
-	}
+		}
+	
 
 	info['trials'] +=1
 	if info['current_slot'] in info['payoffs']:
-		info["payoffs"][info['current_slot']] += state["last-payoff"]
+		info["payoffs"][info['current_slot']].append(state["last-payoff"])
 	else:
-		info["payoffs"][info['current_slot']] = state["last-payoff"]
+		info["payoffs"][info['current_slot']] = [state["last-payoff"]]
 	info['costs'][info['current_slot']] = state['last-cost']
 	info['money'] = info['money'] - state['last-cost'] + state['last-payoff']
-	if(info['trials'] == 49 or (info['costs'][info['current_slot']] * info['trials'] - info['payoffs'][info['current_slot']]) > 5000):
+	
+
+	if(info['trials'] == 49 or (info['costs'][info['current_slot']] * info['trials'] - sum(info['payoffs'][info['current_slot']])/len(info['payoffs'][info['current_slot']]) ) > 5000):
 		info['trials'] =0
 		info['current_slot'] = str(int(info['current_slot']) + 1)
+
 	save_data(info)
+	if (int(info['current_slot']) > 99 or info['state'] == 'max'):
+		info['state'] = 'max'
+		best = get_best(info)[0]
+		info['current_slot'] = str(best)
+		save_data(info)
+		return {"team-code":state["team-code"], 
+			"game":state["game"],
+			"pull":int(best)
+		}
 
 
 	# Return statement
 	return {"team-code":state["team-code"], 
 			"game":state["game"],
-			"pull":info['current_slot']
+			"pull":int(info['current_slot'])
 	}
+
+
+
 
 # Pick best slots from testing phase
 def choose_slots():
 	slot_list = []
 	# Load data
 	info = load_data()
-	ratio = []
-
-	# Get average payoff ratio for each slot
-	for i in range(0,100):
-		ratio.append((info['payoffs'][str(i)] if info['payoffs'][str(i)] != 0 else .0001)/ info["costs"][str(i)])
+	slot_list = get_best(info)
 		
 	
-	# Need to get top 10 ratios
-	slot_list= sorted(range(len(ratio)), key=lambda i: ratio[i])[-15:]
-	slot_list.reverse()
+	
 	info['top_30'] = slot_list[:25]
 	slot_list = slot_list[5:]
 	info['slot_list'] = slot_list
@@ -112,6 +132,8 @@ def play_auction(state):
 
 
 def bid(state,info):
+	if info['money'] < 0:
+		info['money'] = 0
 	#TODO:currently only the simple solution
 	if state['auction-number'] in info['slot_list']:
 		index = info['slot_list'].index(state['auction-number'])
